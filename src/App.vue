dd5f66f669fc81ac4efc8f10b1770b93c1bbd0f3
Nikkei96 <nikitarudentsov@yandex.ru>
2020-09-17 17:24:54 +0300

    init commit v.1.0 full-work

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,92 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class='find'>
+      <b-form-input type="text" name="" id="" 
+        placeholder='IP'
+        class='mb-3'
+        v-model='firstIP'
+        :state='isValid(firstIP)'
+      ></b-form-input>
+      <b-form-input type="text" name="" id="" 
+        placeholder='До'
+        class='mb-3'
+        v-model='secondIP'
+        :state='isValid(secondIP)'
+      ></b-form-input>
+      <b-button 
+        variant="success"
+        size="lg"
+        @click='getListIp(firstIP,secondIP)'
+        class="mb-3"
+      >Найти диапазон</b-button>
+      <b-alert show variant="danger"
+        v-if='fail || fail_last'
+        class='surfacing'>Неверный диапазон!</b-alert>
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      firstIP: '',
+      secondIP: '',
+      fail: false,
+      fail_last: false,
+      regexp : /^(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[0-9]{2}|[0-9])(\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[0-9]{2}|[0-9])){3}$/,
+      resJSON: '',
+    }
+  },
+  methods: {
+    getListIp(ip1, ip2) {
+      let resArr = []
+      let res = []
+      let ipArr1 = ip1.split('.').map(e => +e)
+      let ipArr2 = ip2.split('.').map(e => +e)      
+      for (let i = 0; i <= 2; i++) {
+        if (ipArr1[i] != ipArr2[i] || ipArr1.length != 4) {
+          this.fail = true
+          break;
+        } else {
+          this.fail = false
+          res.push(ipArr1[i])
+        }
+      }
+      if (ipArr1[3] >= ipArr2[3]) {
+        this.fail_last = true
+      } else {
+        this.fail_last = false
+        res.push(ipArr1[3])
+      }
+
+      for (let i = ipArr1[3]; i <= ipArr2[3]; i++) {
+        res[3] = i
+        resArr.push(res.join('.'))
+      }
+
+      if (ip1.length == 0 || ip2.length == 0) {
+        this.fail = true
+      }
+
+      if (!this.fail && !this.fail_last) {
+        // let resJSON = JSON.stringify(resArr)
+        console.log(resArr)
+      }
+
+
+    },
+    isValid(ip) {
+      if (ip == '') {
+        return
+      } else {
+        return ip.match(this.regexp) ? true : false
+      }
+    },
+  },
+  computed: {
+
   }
 }
 </script>
@@ -25,4 +100,33 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.find {
+  border: 1px solid #000;
+  border-radius: 5px;
+  width: 300px;
+  padding: 25px;
+  margin: 0 auto 10px;
+}
+
+.surfacing {
+  animation: bounceIn 0.3s;
+}
+
+@keyframes bounceIn {
+  0% {
+    transform: scale(0.1);
+    opacity: 0;
+  }
+  60% {
+    transform: scale(1.2);
+    opacity: 1;
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+
+
 </style>
